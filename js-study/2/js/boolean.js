9d3796d1d369e9b12f5b86fc120ad2f0c475d75f
jinjintv <theffffff@naver.com>
2024-04-29 20:48:06 +0900

    til 240429 1

---
+++ js-study/2/js/boolean.js
@@ -0,0 +1,9 @@
+var a = true;
+var b = false; // 논리형 a,b
+var c = 10 > 5; // true
+var d = Boolean(null); // false
+// 논리형 데이터 a,b 값 c 비교 연산자 d boolean 메서드
+document.write(a, "<br>");
+document.write(b, "<br>");
+document.write(c, "<br>");
+document.write(d, "<br>");
